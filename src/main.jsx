422bc7feace9fb980f462b4548c49db7c35f645c
Augusto-Zeni <augustozeni955@gmail.com>
2024-10-11 15:18:08 -0300

    add react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Header from './components/Header/Header'
-import PerfilContainer from './containers/PerfilContainer/PerfilContainer'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Home from './routes/Home'
 import GlobalStyles from './styles/GlobalStyles'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={router} />
     <GlobalStyles />
-    <Header />
-    <PerfilContainer />
   </StrictMode>,
 )
---
+++ src/routes/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+   return (
+    <>
+      <h1>Teste Home</h1>
+    </>
+   )
+}
+
+export default Home
